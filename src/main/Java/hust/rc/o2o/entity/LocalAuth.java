3dd6067c698862801f169b241a39c5dce65bce67
Eros Ian <Eros_Ian@outlook.com>
2018-01-27 05:20:23 +0800

    Initial commit

---
+++ src/main/Java/hust/rc/o2o/entity/LocalAuth.java
@@ -0,0 +1,70 @@
+/**
+ * Description:本地账号
+ * File Name:LocalAuth
+ * Date:2018/1/26
+ * Time:17:18
+ * Copyright (c) 2018 - Eros_Ian@outlook.com All Rights Reserved.
+ */
+package hust.rc.o2o.entity;
+
+public class LocalAuth {
+    private Long localAuthId;
+    //用户名
+    private String username;
+    //密码
+    private String password;
+    //创建时间
+    private String createTime;
+    //最后编辑时间
+    private String lastEditTime;
+    //用户实体类
+    private PersonInfo personInfo;
+
+    public Long getLocalAuthId() {
+        return localAuthId;
+    }
+
+    public void setLocalAuthId(Long localAuthId) {
+        this.localAuthId = localAuthId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(String lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
---
+++ src/main/Java/hust/rc/o2o/entity/WechatAuth.java
@@ -0,0 +1,52 @@
+/**
+ * Description:微信账号
+ * File Name:WechatAuth
+ * Date:2018/1/26
+ * Time:17:17
+ * Copyright (c) 2018 - Eros_Ian@outlook.com All Rights Reserved.
+ */
+package hust.rc.o2o.entity;
+
+import java.util.Date;
+
+public class WechatAuth {
+    private Long wechatAuthId;
+    //openID
+    private String openID;
+    //创建时间
+    private Date createTime;
+    //用户实体类
+    private PersonInfo personInfo;
+
+    public Long getWechatAuthId() {
+        return wechatAuthId;
+    }
+
+    public void setWechatAuthId(Long wechatAuthId) {
+        this.wechatAuthId = wechatAuthId;
+    }
+
+    public String getOpenID() {
+        return openID;
+    }
+
+    public void setOpenID(String openID) {
+        this.openID = openID;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
