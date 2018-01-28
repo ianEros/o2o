30bc970ba09b12f35463d94daa55df5cc07222cf
Eros Ian <Eros_Ian@outlook.com>
2018-01-29 02:08:36 +0800

    2018年1月29日02点06分

---
+++ src/main/Java/hust/rc/o2o/entity/ProductCategory.java
@@ -0,0 +1,58 @@
+/*
+ * Description:商品类别
+ * File Name:ProductCategory
+ * Date:2018/1/28
+ * Time:18:38
+ * Copyright (c) 2018 - Eros_Ian@outlook.com All Rights Reserved.
+ */
+package hust.rc.o2o.entity;
+
+import java.util.Date;
+
+public class ProductCategory {
+    private Long productCategoryId;
+    private Long shopId;
+    private String productCategoryName;
+    private Integer priority;
+    private Date createTime;
+
+    public Long getProductCategoryId() {
+        return productCategoryId;
+    }
+
+    public void setProductCategoryId(Long productCategoryId) {
+        this.productCategoryId = productCategoryId;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getProductCategoryName() {
+        return productCategoryName;
+    }
+
+    public void setProductCategoryName(String productCategoryName) {
+        this.productCategoryName = productCategoryName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
